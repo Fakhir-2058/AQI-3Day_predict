9ef84f92fae31ac27902ec0e7c6dc05df4283fb3
Fakhir-2058 <fakhir.ahmed2058@gmail.com>
2026-09-03 17:14:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import requests
 import streamlit as st
 
-API_URL = os.environ.get("AQI_API_URL", "API_BASE_URL/").rstrip("/")
+API_URL = os.environ.get("AQI_API_URL", "https://aqi-3-day-predict-mu.vercel.app/").rstrip("/")
 REFRESH_SECONDS = int(os.environ.get("AQI_REFRESH_SECONDS", "60"))
 
 STATUS_COLORS = {
