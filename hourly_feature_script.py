8b33ea9e3f8cc2e9fb2ec347c44f9863e60daff5
Fakhir-2058 <fakhir.ahmed2058@gmail.com>
2026-09-03 15:51:29 +0500

    Update hourly_feature_script.py

--- hourly_feature_script.py
+++ hourly_feature_script.py
@@ -1,36 +1,235 @@
+# import requests
+# import pandas as pd
+# import hopsworks
+# import os
+
+# def get_geocodeinfo(name):
+
+#     url = f"https://geocoding-api.open-meteo.com/v1/search?name={name}&count=10&language=en&format=json"
+
+#     response = requests.get(url, timeout=30)
+#     response.raise_for_status()
+
+#     return response.json()
+
+
+# def get_AQI_info(lat, lon, start_date, end_date):
+
+#     url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,european_aqi,us_aqi"
+
+#     response = requests.get(url, timeout=60)
+#     response.raise_for_status()
+
+#     return response.json()
+
+
+# def get_weatherinfo(lat, lon, start_date, end_date):
+
+#     url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,surface_pressure,wind_speed_10m"
+
+#     response = requests.get(url, timeout=60)
+#     response.raise_for_status()
+
+#     return response.json()
+
+# project = hopsworks.login(
+#     api_key_value=os.environ["HOPSWORKS_API_KEY"]
+# )
+
+# print("Connected to Hopsworks!")
+
+# feature_store = project.get_feature_store()
+
+# print("Feature Store loaded!")
+
+
+
+# #  LOAD HOURLY FEATURE GROUP
+
+
+# feature_group = feature_store.get_feature_group(
+#     name="aqi_hourly",
+#     version=1
+# )
+
+# if feature_group is None:
+#     raise Exception("Feature Group 'aqi_hourly' version 1 was not found.")
+
+
+# print("Hourly Feature Group loaded!")
+
+# existing_df = feature_group.read()
+
+# print("Existing hourly data loaded!")
+# print("Existing hourly shape:", existing_df.shape)
+
+# existing_df["time"] = pd.to_datetime(existing_df["time"])
+# existing_df = existing_df.sort_values("time").reset_index(drop=True)
+
+
+# latest_existing_time = existing_df["time"].max()
+# print("Latest hour already in Feature Group:",latest_existing_time)
+
+# start_date = (latest_existing_time- pd.Timedelta(days=3)).strftime("%Y-%m-%d")
+# end_date = pd.Timestamp.today().strftime("%Y-%m-%d")
+
+
+# print("Downloading hourly data from:", start_date)
+# print("Downloading hourly data until:", end_date)
+
+
+# city_name = "Lahore"
+
+# geocode_info = get_geocodeinfo(city_name)
+
+# if "results" not in geocode_info:
+#     raise Exception("City not found!")
+
+# latitude = geocode_info["results"][0]["latitude"]
+# longitude = geocode_info["results"][0]["longitude"]
+
+# print("City:", city_name)
+# print("Latitude:", latitude)
+# print("Longitude:", longitude)
+
+# weather = get_weatherinfo(latitude,longitude,start_date,end_date)
+# aqi = get_AQI_info(latitude,longitude,start_date,end_date)
+
+# aqi_hourly = aqi["hourly"]
+# weather_hourly = weather["hourly"]
+
+# times = weather_hourly["time"]
+
+# combined_data = []
+
+
+# for i in range(len(times)):
+
+#     row = {
+#         "time": times[i],
+
+#         "temperature": weather_hourly["temperature_2m"][i],
+#         "humidity": weather_hourly["relative_humidity_2m"][i],
+#         "pressure": weather_hourly["surface_pressure"][i],
+#         "wind_speed": weather_hourly["wind_speed_10m"][i],
+#         "pm2_5": aqi_hourly["pm2_5"][i],
+#         "pm10": aqi_hourly["pm10"][i],
+#         "co": aqi_hourly["carbon_monoxide"][i],
+#         "no2": aqi_hourly["nitrogen_dioxide"][i],
+#         "so2": aqi_hourly["sulphur_dioxide"][i],
+#         "ozone": aqi_hourly["ozone"][i],
+#         "european_aqi": aqi_hourly["european_aqi"][i],
+#         "us_aqi": aqi_hourly["us_aqi"][i]
+#     }
+
+#     combined_data.append(row)
+
+
+
+# hourly_data = pd.DataFrame(combined_data)
+
+# hourly_data["time"] = pd.to_datetime(hourly_data["time"])
+# hourly_data = hourly_data.dropna().reset_index(drop=True)
+# hourly_data = hourly_data.drop_duplicates(subset=["time"])
+
+# hourly_data = hourly_data.sort_values("time").reset_index(drop=True)
+
+
+# print("HOURLY DATA CREATED")
+
+# print(hourly_data.head())
+# print(hourly_data.tail())
+
+# print("\nHourly data shape:", hourly_data.shape)
+
+# existing_times = set(existing_df["time"])
+
+# new_data = hourly_data[~hourly_data["time"].isin(existing_times)].copy()
+
+
+# print("New records found:", len(new_data))
+
+# if len(new_data) > 0:
+
+#     print("Inserting new hourly data...")
+
+#     feature_group.insert(new_data,
+#         write_options={
+#             "wait_for_job": True
+#         }
+#     )
+#     print("NEW HOURLY DATA INSERTED SUCCESSFULLY!")
+
+# else:
+#     print("NO NEW DATA TO INSERT")
+  
+
+
+# print("\nPipeline finished successfully!")
+
+
+
+
+
+
+
 import requests
 import pandas as pd
 import hopsworks
 import os
+import time
+
 
 def get_geocodeinfo(name):
 
     url = f"https://geocoding-api.open-meteo.com/v1/search?name={name}&count=10&language=en&format=json"
 
-    response = requests.get(url, timeout=30)
-    response.raise_for_status()
+    for i in range(3):
+        try:
+            response = requests.get(url, timeout=60)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print("Geocoding error:", e)
+            if i < 2:
+                time.sleep(5)
 
-    return response.json()
+    raise Exception("Geocoding API failed")
 
 
 def get_AQI_info(lat, lon, start_date, end_date):
 
     url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,european_aqi,us_aqi"
 
-    response = requests.get(url, timeout=60)
-    response.raise_for_status()
+    for i in range(3):
+        try:
+            response = requests.get(url, timeout=180)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print("AQI API error:", e)
+            if i < 2:
+                time.sleep(5)
 
-    return response.json()
+    raise Exception("AQI API failed")
 
 
 def get_weatherinfo(lat, lon, start_date, end_date):
 
     url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,surface_pressure,wind_speed_10m"
 
-    response = requests.get(url, timeout=60)
-    response.raise_for_status()
+    for i in range(3):
+        try:
+            response = requests.get(url, timeout=180)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print("Weather API error:", e)
+            if i < 2:
+                time.sleep(5)
 
-    return response.json()
+    raise Exception("Weather API failed")
+
 
 project = hopsworks.login(
     api_key_value=os.environ["HOPSWORKS_API_KEY"]
@@ -43,10 +242,6 @@ feature_store = project.get_feature_store()
 print("Feature Store loaded!")
 
 
-
-#  LOAD HOURLY FEATURE GROUP
-
-
 feature_group = feature_store.get_feature_group(
     name="aqi_hourly",
     version=1
@@ -54,7 +249,6 @@ feature_group = feature_store.get_feature_group(
 
 if feature_group is None:
     raise Exception("Feature Group 'aqi_hourly' version 1 was not found.")
-
 
 print("Hourly Feature Group loaded!")
 
@@ -64,15 +258,31 @@ print("Existing hourly data loaded!")
 print("Existing hourly shape:", existing_df.shape)
 
 existing_df["time"] = pd.to_datetime(existing_df["time"])
-existing_df = existing_df.sort_values("time").reset_index(drop=True)
 
+existing_df = existing_df.sort_values(
+    "time"
+).reset_index(drop=True)
 
 latest_existing_time = existing_df["time"].max()
-print("Latest hour already in Feature Group:",latest_existing_time)
 
-start_date = (latest_existing_time- pd.Timedelta(days=3)).strftime("%Y-%m-%d")
-end_date = pd.Timestamp.today().strftime("%Y-%m-%d")
+print(
+    "Latest hour already in Feature Group:",
+    latest_existing_time
+)
 
+
+# start from the next missing hour
+start_time = latest_existing_time + pd.Timedelta(hours=1)
+
+current_time = pd.Timestamp.utcnow().floor("h").tz_localize(None)
+
+if start_time > current_time:
+    print("No new data available.")
+    print("\nPipeline finished successfully!")
+    raise SystemExit(0)
+
+start_date = start_time.strftime("%Y-%m-%d")
+end_date = current_time.strftime("%Y-%m-%d")
 
 print("Downloading hourly data from:", start_date)
 print("Downloading hourly data until:", end_date)
@@ -92,47 +302,76 @@ print("City:", city_name)
 print("Latitude:", latitude)
 print("Longitude:", longitude)
 
-weather = get_weatherinfo(latitude,longitude,start_date,end_date)
-aqi = get_AQI_info(latitude,longitude,start_date,end_date)
+
+weather = get_weatherinfo(
+    latitude,
+    longitude,
+    start_date,
+    end_date
+)
+
+aqi = get_AQI_info(
+    latitude,
+    longitude,
+    start_date,
+    end_date
+)
+
 
 aqi_hourly = aqi["hourly"]
 weather_hourly = weather["hourly"]
 
-times = weather_hourly["time"]
 
-combined_data = []
-
-
-for i in range(len(times)):
-
-    row = {
-        "time": times[i],
-
-        "temperature": weather_hourly["temperature_2m"][i],
-        "humidity": weather_hourly["relative_humidity_2m"][i],
-        "pressure": weather_hourly["surface_pressure"][i],
-        "wind_speed": weather_hourly["wind_speed_10m"][i],
-        "pm2_5": aqi_hourly["pm2_5"][i],
-        "pm10": aqi_hourly["pm10"][i],
-        "co": aqi_hourly["carbon_monoxide"][i],
-        "no2": aqi_hourly["nitrogen_dioxide"][i],
-        "so2": aqi_hourly["sulphur_dioxide"][i],
-        "ozone": aqi_hourly["ozone"][i],
-        "european_aqi": aqi_hourly["european_aqi"][i],
-        "us_aqi": aqi_hourly["us_aqi"][i]
-    }
-
-    combined_data.append(row)
+weather_df = pd.DataFrame({
+    "time": weather_hourly["time"],
+    "temperature": weather_hourly["temperature_2m"],
+    "humidity": weather_hourly["relative_humidity_2m"],
+    "pressure": weather_hourly["surface_pressure"],
+    "wind_speed": weather_hourly["wind_speed_10m"]
+})
 
 
+aqi_df = pd.DataFrame({
+    "time": aqi_hourly["time"],
+    "pm2_5": aqi_hourly["pm2_5"],
+    "pm10": aqi_hourly["pm10"],
+    "co": aqi_hourly["carbon_monoxide"],
+    "no2": aqi_hourly["nitrogen_dioxide"],
+    "so2": aqi_hourly["sulphur_dioxide"],
+    "ozone": aqi_hourly["ozone"],
+    "european_aqi": aqi_hourly["european_aqi"],
+    "us_aqi": aqi_hourly["us_aqi"]
+})
 
-hourly_data = pd.DataFrame(combined_data)
 
-hourly_data["time"] = pd.to_datetime(hourly_data["time"])
-hourly_data = hourly_data.dropna().reset_index(drop=True)
-hourly_data = hourly_data.drop_duplicates(subset=["time"])
+weather_df["time"] = pd.to_datetime(weather_df["time"])
+aqi_df["time"] = pd.to_datetime(aqi_df["time"])
 
-hourly_data = hourly_data.sort_values("time").reset_index(drop=True)
+
+# combine both datasets using time
+hourly_data = pd.merge(
+    weather_df,
+    aqi_df,
+    on="time",
+    how="inner"
+)
+
+
+hourly_data = hourly_data[
+    (hourly_data["time"] > latest_existing_time) &
+    (hourly_data["time"] <= current_time)
+].copy()
+
+
+hourly_data = hourly_data.dropna()
+
+hourly_data = hourly_data.drop_duplicates(
+    subset=["time"]
+)
+
+hourly_data = hourly_data.sort_values(
+    "time"
+).reset_index(drop=True)
 
 
 print("HOURLY DATA CREATED")
@@ -142,27 +381,28 @@ print(hourly_data.tail())
 
 print("\nHourly data shape:", hourly_data.shape)
 
-existing_times = set(existing_df["time"])
+print(
+    "New records found:",
+    len(hourly_data)
+)
 
-new_data = hourly_data[~hourly_data["time"].isin(existing_times)].copy()
 
-
-print("New records found:", len(new_data))
-
-if len(new_data) > 0:
+if len(hourly_data) > 0:
 
     print("Inserting new hourly data...")
 
-    feature_group.insert(new_data,
+    feature_group.insert(
+        hourly_data,
         write_options={
             "wait_for_job": True
         }
     )
+
     print("NEW HOURLY DATA INSERTED SUCCESSFULLY!")
 
 else:
+
     print("NO NEW DATA TO INSERT")
-  
 
 
 print("\nPipeline finished successfully!")
